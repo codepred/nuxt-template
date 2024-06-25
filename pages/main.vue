ce900b9e19b77b0599481abb285f6eacf9ee1a7d
Mateusz Stawski <mateuszmstawski@gmail.com>
2024-06-25 22:20:03 +0200

    feat: introduce search feature to highlight matching text in inputs

--- pages/main.vue
+++ pages/main.vue
@@ -4,6 +4,11 @@ import { useFormStore } from '~/store/form'
 const isStoreLoaded = ref(false)
 const formStore = useFormStore()
 
+const isSearchMatch = computed(() => {
+    if (!formStore.searchText) return false;
+    return formStore.fields.some(field => field.value.includes(formStore.searchText));
+});
+
 watch(formStore, () => {
     if (formStore.fields) {
         isStoreLoaded.value = true
@@ -18,6 +23,15 @@ watch(formStore, () => {
             class="overflow-y-auto pb-8"
             style="max-height: 500px;"
         >
+            <div>Search:
+                <v-text-field
+                    v-model="formStore.searchText"
+                    @input="formStore.setSearchText(formStore.searchText)"
+                    append-inner-icon="mdi-magnify"
+                    hide-details
+                    :class="{ highlight: isSearchMatch }"
+                ></v-text-field>
+            </div>
             <v-list lines="two">
                 <v-list-item
                     v-for="(line, index) in formStore.fields"
@@ -29,6 +43,7 @@ watch(formStore, () => {
                             v-model="line.value"
                             hide-details
                             @input="formStore.updateField(line.id, line.value)"
+                            :class="{ highlight: formStore.searchText && line.value.includes(formStore.searchText) }"
                         >
                         </v-text-field>
                         <span>
@@ -42,7 +57,7 @@ watch(formStore, () => {
             </v-list>
             <div
                 class="d-flex ga-2 cursor-pointer w-fit-content add-new-row-container"
-                @click="formStore.addField" 
+                @click="formStore.addField"
             >
                 <v-icon :disabled="formStore.fields.length >= 10">
                     mdi-plus
@@ -67,6 +82,10 @@ watch(formStore, () => {
     margin: 0 auto;
     border-radius: 12px;
     padding: 16px;
+}
+
+.highlight {
+    background-color: #4BB543;
 }
 
 @media (max-width: 350px) {
